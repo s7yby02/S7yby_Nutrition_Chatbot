4380a8cfd9baed71dbb3aea3dd4e550551d78000
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2023-11-21 10:11:59 +0100

    first commit

---
+++ chatbot/src/test/java/com/jee_project/chatbot/ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jee_project.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
