e10e4865de77016cffeca745991e7dbf2d106d56
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2023-11-28 00:44:19 +0100

    backend security

--- chatbot/src/main/java/com/jee_project/chatbot/ApplicationSecurityConfig.java
+++ chatbot/src/main/java/com/jee_project/chatbot/ApplicationSecurityConfig.java
@@ -1,5 +1,46 @@
 package com.jee_project.chatbot;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 public class ApplicationSecurityConfig {
     
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+ 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+
+                    .requestMatchers("/login","/img/**","/css/**").permitAll()
+                    .anyRequest().authenticated())
+            .formLogin(formLogin -> formLogin
+                    .loginPage("/login")
+                    .permitAll())
+            .logout(logout -> logout.invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login")
+                    .permitAll());
+
+        return http.build();
+    }
+ 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    }
 }
