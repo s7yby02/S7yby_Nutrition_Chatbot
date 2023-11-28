8f55040fa9292142dc8b4c8aa6365c0e5829490b
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2023-11-28 03:02:26 +0100

    back sec

--- chatbot/src/main/java/com/jee_project/chatbot/ApplicationSecurityConfig.java
+++ chatbot/src/main/java/com/jee_project/chatbot/ApplicationSecurityConfig.java
@@ -1,13 +1,19 @@
 package com.jee_project.chatbot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
 @EnableWebSecurity
@@ -22,13 +28,15 @@ public class ApplicationSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
 
-                    .requestMatchers("/login","/img/**","/css/**").permitAll()
+                    .requestMatchers("/login","/ressources/**","/assets/img/**","/assets/css/**","/assets/js/**","/assets/vendor/**").permitAll()
+                    .requestMatchers("/register","/ressources/**","/assets/img/**","/assets/css/**","/assets/js/**","/assets/vendor/**").permitAll()
+                    .requestMatchers("/users/addNew").permitAll()
                     .anyRequest().authenticated())
             .formLogin(formLogin -> formLogin
                     .loginPage("/login")
+                    .usernameParameter("username")
                     .permitAll())
             .logout(logout -> logout.invalidateHttpSession(true)
                     .clearAuthentication(true)
@@ -38,9 +46,27 @@ public class ApplicationSecurityConfig {
 
         return http.build();
     }
- 
+
+   
+    
+    //@Bean
+    //public PasswordEncoder passwordEncoder(){
+    //    return NoOpPasswordEncoder.getInstance();
+    //}
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    //@Bean
+    //public WebSecurityCustomizer webSecurityCustomizer() {
+    //    return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    //}
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
