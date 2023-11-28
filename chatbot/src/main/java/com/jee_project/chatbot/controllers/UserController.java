933320992407a64f39084c6d01b17a8f47417586
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2023-11-28 04:38:01 +0100

    backdev sec

--- chatbot/src/main/java/com/jee_project/chatbot/controllers/UserController.java
+++ chatbot/src/main/java/com/jee_project/chatbot/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.jee_project.chatbot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
@@ -15,7 +16,7 @@ public class UserController {
     @Autowired private UserService userService;
 
     @PostMapping(value="/users/addNew")
-    public RedirectView addNew(User user, RedirectAttributes redir){
+    public RedirectView addNew(@ModelAttribute User user, RedirectAttributes redir){
         userService.save(user);
 
         RedirectView redirectView = new RedirectView("/login", true);
--- chatbot/src/main/java/com/jee_project/chatbot/services/UserService.java
+++ chatbot/src/main/java/com/jee_project/chatbot/services/UserService.java
@@ -1,6 +1,7 @@
 package com.jee_project.chatbot.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jee_project.chatbot.models.User;
@@ -9,9 +10,11 @@ import com.jee_project.chatbot.repositories.UserRepository;
 @Service
 public class UserService {
     
+    @Autowired private BCryptPasswordEncoder encoder;
     @Autowired private UserRepository userRepository;
     
     public void save(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         userRepository.save(user);   
     }
 }
